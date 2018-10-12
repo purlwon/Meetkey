082d1737639c0d439d0acad0a44b9e97b36e6a7d
Oh, Jeonghyeon <yoosoo615@gmail.com>
2018-10-13 00:55:41 +0900

    add api for dashboard and add function in cookie.js(time_all)

--- production/js/cookie.js
+++ production/js/cookie.js
@@ -2,3 +2,17 @@ var getCookie = function(name) {
   var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
   return value? value[2] : null;
 };
+
+var TimeAll2String = function(time) {
+  var tmp = time / 1000
+  var second = parseInt(tmp % 60);
+  tmp = tmp / 60;
+  var minute = parseInt(tmp % 60);
+  tmp = tmp / 60;
+  var hour = parseInt(tmp % 60);
+
+  if(hour > 0)
+    return hour+"시 "+minute+"분 "+second+"초"
+  else
+    return minute+"분 "+second+"초"
+}
